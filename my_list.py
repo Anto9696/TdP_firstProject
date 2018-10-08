6c655f37f93ef53bf0b3328f8cedbd2273467985
Anto9696 <vgyu>
2018-10-08 19:44:09 +0200

    Start from here

--- my_list.py
+++ my_list.py
@@ -8,7 +8,6 @@ class MyList:
             self._prev = prev  # previous node reference
             self._next = next  # next node reference
 
-
     __slots__ = '_size', '_header', '_trailer'
 
     def __init__(self):
