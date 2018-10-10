72b9ea4dc39643e1afb504492a8da8adbae89b4b
Anto9696 <vgyu>
2018-10-10 16:48:44 +0200

    Start from here

---
+++ esercizio2.py
@@ -0,0 +1,42 @@
+from my_list import MyList
+
+class MyList_Bubbled(MyList):
+
+    def bubblesorted(self):
+        list_ordered=self.copy()
+        sup=len(self)
+        while sup!=0:
+            last_swap = 0
+            cursor=list_ordered.first()
+            succ_cursor=list_ordered.after(cursor)
+            for i in range(sup-1):
+                if cursor.element() > succ_cursor.element():
+                    list_ordered.replace(succ_cursor,list_ordered.replace(cursor,succ_cursor.element()))
+                    last_swap = i
+            sup=last_swap
+
+        for element in list_ordered:
+            yield element
+
+if __name__=="__main__":
+    list1 = MyList_Bubbled()
+    list2 = MyList_Bubbled()
+    for i in range(10):
+        list1.add_first(i)
+        list2.add_last(i+11)
+
+    print("LIST 1")
+    for e in list1:
+        print(e)
+    print("LIST 2")
+    for e in list2:
+        print(e)
+
+    list3 = list1 + list2
+    print("LIST 3")
+    for e in list3:
+        print(e)
+
+    print("ORDERED LIST 3")
+    for e in list3.bubblesorted():
+        print(e)
