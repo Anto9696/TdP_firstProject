1d771865e81d554816695ed58a33dc3f57f31d24
Mario Mupo <mario.mupo@outlook.com>
2018-10-10 20:00:59 +0200

    is_sorted

--- my_list.py
+++ my_list.py
@@ -38,7 +38,7 @@ class MyList(PositionalList):
 
     def is_sorted(self):
         """restituisce True se la lista è ordinata e False altrimenti"""
-        current_node = self.first()._node
+        current_node = self._header
 
         for i in range(len(self)-1):
             if current_node._element < current_node._next._element:
