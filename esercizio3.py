5c6cfd1016a16bf0a4c246ecf7abf0b2da16d180
Romeo1996 <romeo.rinaldi1996@gmail.com>
2018-10-10 19:17:13 +0200

    merge

--- esercizio3.py
+++ esercizio3.py
@@ -6,14 +6,16 @@ def merge(list1,list2):
     e=i=0
     while(i<list1.__len__() and e<list2.__len__() ):
         if(current_first_list._element>current_second_list._element):
-            list1.add_before(list1.make_position(current_first_list),current_second_list._element)
+            list1.add_before(list1._make_position(current_first_list),current_second_list._element)
             current_second_list=current_second_list._next
             e+=1
         else:
-            current_first_list=current_first_list
+            current_first_list=current_first_list._next
             i+=1
     while (e<list2.__len__()):
-        list1.add_before(list1.make_position(current_first_list),current_second_list.element())
+        list1.add_last(current_second_list._element)
+        current_second_list=current_second_list._next
+        e+=1
 
 if __name__=="__main__":
     list1 = MyList()
@@ -23,7 +25,8 @@ if __name__=="__main__":
             list1.add_last(i)
         else:
             list2.add_last(i)
-
+    list1.add_last(23)
+    list1.add_last(24)
     print("LIST 1")
     for e in list1:
         print(e)
