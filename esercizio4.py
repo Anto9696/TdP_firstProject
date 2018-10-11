98f3c2910c46765d31d8268b16145c4a7500e6a8
Mario Mupo <mario.mupo@outlook.com>
2018-10-11 11:16:46 +0200

    4

--- esercizio4.py
+++ esercizio4.py
@@ -3,14 +3,21 @@ from esercizio2 import bubblesorted
 from esercizio3 import merge
 
 
-class ScoreBoard:
-    class Score:
-        def __init__(self):
-            self._player = None
-            self._score = 0
-            self._date = "00/00/0000"
+class Score:
+    def __init__(self):
+        self._player = None
+        self._score = 0
+        self._date = "00/00/0000"
 
-    def __init__(self,x):
+    def add_element(self, pl, sc, da):
+        self._player = pl
+        self._score = sc
+        self._date = da
+
+
+class ScoreBoard:
+
+    def __init__(self, x):
         self._best = MyList()
         self._max = x
         self._size = 0
@@ -26,20 +33,20 @@ class ScoreBoard:
         correntemente salvati. Non incrementa la dimensione dello scoreboard"""
         if len(self) < self._max:
             score = self._best._header
-            for i in range(len(self)-1):
+            for i in range(len(self) - 1):
                 if s._score < score:
                     raise TypeError("Too bad score")
                 self._best.add_last(s)
                 self._size += 1
         # raise NotImplementedError("Not implemented")
 
-    def merge(self,new):
+    def merge(self, new):
         """Fonde lo scoreboard corrente con new selezionando i 10 migliori risultati"""
         merge(self._best, new)
         return self.top(10)
         # raise NotImplementedError("Not implemented")
 
-    def top(self,i):
+    def top(self, i):
         """Restituisce i migliori i score nello ScoreBoard"""
         imp = len(self._best) - i
         counter = 0
@@ -49,7 +56,7 @@ class ScoreBoard:
             counter += 1
         # raise NotImplementedError("Not implemented")
 
-    def last(self,i):
+    def last(self, i):
         """Restituisce i peggiori i score nello ScoreBoard"""
         counter = 0
         for el in bubblesorted(self._best):
@@ -59,3 +66,15 @@ class ScoreBoard:
             else:
                 break
         # raise NotImplementedError("Not implemented")
+
+
+if __name__ == "__main__":
+    score1 = score2 = score3 = Score()
+    score1.add_element("Gino", 10, "15/10/2017")
+    # print(score1._player, " ", score1._score, " ", score1._date)
+    score2.add_element("AAA", 5, "15/10/2017")
+    score3.add_element("BBB", 15, "15/10/2017")
+
+    SB = ScoreBoard(4)
+    print(SB.is_empty()) # verifico is_empty
+
