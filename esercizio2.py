21e9b29985216bc807b20e2d575c5b21a79eacb9
Gerry <g>
2018-10-20 16:07:55 +0200

    gerry

--- esercizio2.py
+++ esercizio2.py
@@ -2,21 +2,22 @@ from CircularPositionalList import CircularPositionalList
 
 
 def bubblesorted(list):
-    """ordina gli elementi della CircularPositionalList e
-    li restituisce nell’ordine risultante."""
+    """ordina gli elementi della CircularPositionalList e li restituisce nell’ordine risultante.
+       Non modifica l'ordine di memorizzazione degli elementi nella lista passatagli."""
     list_ordered = list.copy()
-    sup = len(list_ordered) - 1
-    while sup != 0:
-        last_swap = 0
-        cursor = list_ordered.first()
-        succ_cursor = list_ordered._next_position(cursor)
-        for i in range(sup):
-            if cursor.element() > succ_cursor.element():
-                list_ordered.replace(succ_cursor, list_ordered.replace(cursor, succ_cursor.element()))
-                last_swap = i
-            cursor = succ_cursor
+    if not list_ordered.is_empty():
+        sup = len(list_ordered) - 1
+        while sup != 0:                #perchè se list aveva meno di 2 elem
+            last_swap = 0
+            cursor = list_ordered.first()
             succ_cursor = list_ordered._next_position(cursor)
-        sup = last_swap
+            for i in range(sup):
+                if cursor.element() > succ_cursor.element():
+                    list_ordered.replace(succ_cursor, list_ordered.replace(cursor, succ_cursor.element()))
+                    last_swap = i
+                cursor = succ_cursor
+                succ_cursor = list_ordered._next_position(cursor)
+            sup = last_swap
 
     for element in list_ordered:
         yield element
@@ -32,11 +33,22 @@ if __name__ == "__main__":
 
     print("LIST 1")
     print(list1)
+    print("ORDERED LIST 1")
+    out = ""
+    for e in bubblesorted(list1):
+        out += str(e) + ", "
+    print(out[:-2])
+
     print("LIST 2")
     print(list2)
+    print("ORDERED LIST 2")
+    out = ""
+    for e in bubblesorted(list2):
+        out += str(e) + ", "
+    print(out[:-2])
 
     list3 = list1 + list2
-    print("LIST 3")
+    print("LIST 3 (is list1 + list2)")
     print(list3)
 
     print("ORDERED LIST 3")
@@ -45,5 +57,26 @@ if __name__ == "__main__":
         out += str(e) + ", "
     print(out[:-2])
 
-    print("LIST 3")
-    print(list3)
+    list4 = CircularPositionalList()
+    print("LIST 4 is an empty list")
+    print("ORDERED LIST 4")
+    out = ""
+    for e in bubblesorted(list4):
+        out += str(e) + ", "
+    if not out:
+        print("[empty]")
+
+    list4.add_first(3)
+    print("ADDED 3 AT LIST 4")
+    print("ORDERED LIST 3")
+    out = ""
+    for e in bubblesorted(list4):
+        out += str(e) + ", "
+    print(out[:-2])
+
+
+
+
+
+
+
